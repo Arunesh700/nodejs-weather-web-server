d38b6a7574d70f1731ca1263dcf7729c77049e80
unknown <aruneshdhar72@gmail.com>
2020-06-01 11:52:53 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const cast =  require('./utils/forecast');
 // console.log(__dirname);
 // console.log(path.join(__dirname,'../public'));
 const app = express();
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname,'../public')
 app.set('views',path.join(__dirname,'../templates/views'));
 const partialsPath = path.join(__dirname,'../templates/partials');
@@ -88,6 +89,6 @@ app.get('*', (req,res) => {
       name:'Arunesh'
   })
 })
-app.listen(3000,() => {
-  console.log('Server is up on port 3000');
+app.listen(port,() => {
+  console.log('Server is up on port' + port);
 })
