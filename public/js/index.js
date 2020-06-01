189b838141677f66238cee543b511b3240654cf1
unknown <aruneshdhar72@gmail.com>
2020-06-01 12:07:45 +0530

    Loading setup

--- public/js/index.js
+++ public/js/index.js
@@ -4,9 +4,11 @@ console.log('client side js is loaded');
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const para = document.querySelectorAll('p');
+const load = document.querySelector('.load');
 console.log(para);
 weatherForm.addEventListener('submit',(e)=> {
   e.preventDefault();
+  load.style.display = "block";
   console.log(search.value);
   fetch('/weather?address=' + search.value)
   .then(res => res.json())
@@ -19,6 +21,7 @@ weatherForm.addEventListener('submit',(e)=> {
       }else {
   console.log(data.forecast);
   console.log(data.temperature);
+  load.style.display = "none";
   para[0].textContent = data.forecast;
   para[1].textContent = 'Here in ' + data.location + ' The temperature is ' + data.temperature + 'but it feels like ' + data.feelslike;
 
